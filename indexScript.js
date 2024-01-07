c089d3b668731518fe5c661d47b761842b88f202
medicine-for-students <152507197+medicineForStudents@users.noreply.github.com>
2024-01-07 10:19:22 +0200

    dfe

---
+++ indexScript.js
@@ -0,0 +1,11 @@
+
+let button = document.getElementById('entryHeaderBttn');
+   
+    
+button.addEventListener('click', function buttonClicked(){
+   window.location.href = 'entryPage.html'
+   // Изпълнение на функцията при зареждане на страницата
+   
+ })
+ buttonClicked();
+ 
